c1125c85673739252cf11df49d3315ad3d74c9d8
Kirill Chernyshov <delaguardo@gmail.com>
2018-07-02 16:29:24 +0200

    Add main function

--- src/main/xapix/soap_clj/core.clj
+++ src/main/xapix/soap_clj/core.clj
@@ -1,8 +1,12 @@
 (ns xapix.soap-clj.core
-  (:require [xapix.soap-clj.wsdl-parser :as wsdl-parser]))
+  (:require [xapix.soap-clj.wsdl-parser :as wsdl-parser]
+            [cheshire.core :refer [generate-string]]))
 
 (defn decompose-wsdl [wsdl-path]
   (wsdl-parser/parse-wsdl wsdl-path))
+
+(defn -main [& args]
+  (println (generate-string (wsdl-parser/parse-wsdl (first args)) {:pretty true})))
 
 (comment
 
