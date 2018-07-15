9b01af66f03353313642746be152adc847cc5bf4
Kirill Chernyshov <delaguardo@gmail.com>
2018-07-15 19:00:16 +0200

    Add -main function into paos.core for cli using

--- src/paos/core.clj
+++ src/paos/core.clj
@@ -1,21 +1,59 @@
 (ns paos.core
   (:require [paos.wsdl :as wsdl]
-            [clojure.pprint :refer [pprint]]
-            [clojure.java.io :as io]
-            [clojure.xml :as xml]
-            [clojure.zip :as zip]
-            [cheshire.core :as cheshire]
-            [clojure.data.zip.xml :as zip-xml]))
+            [paos.service :as service]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.pprint :as pprint]))
 
-(defn decompose-wsdl [wsdl-path]
-  (wsdl/parse wsdl-path))
+(def cli-options
+  [["-w" "--wsdl WSDL" "Url or filesystem path or a content of wsdl. (Required)"
+    :parse-fn str
+    :validate [#(string? (not-empty %)) "Must be a non-empty string."]]
+   ["-b" "--binding BINDING" "One of SOAP binding defined in WSDL."
+    :parse-fn str]
+   ["-o" "--operation OPERATION" "One of SOAP operation defined for given WSDL binding. Can not be used without binding."
+    :parse-fn str]
+   ["-h" "--help" "Display this help message"]])
+
+(defn- help
+  [args]
+  (println "\nUSAGE:\n")
+  (println "clj -m" (namespace `help) "<options>\n")
+  (println (:summary args)))
+
+(defn- print-table [bindings]
+  (pprint/print-table
+   (mapcat (fn [[binding {:keys [operations]}]]
+             (map (fn [[operation _]]
+                    {:binding binding
+                     :operation operation})
+                  operations))
+           bindings)))
 
 (defn -main [& args]
-  (println (cheshire/generate-string (wsdl/parse (first args)) {:pretty true})))
-
-(comment
-
-  (def x
-    (wsdl/parse "resources/airlinesService.xml"))
-
-  x)
+  (let [args (parse-opts args cli-options)]
+    (if (:errors args)
+      (do (doseq [e (:errors args)]
+            (println e))
+          (help args))
+      (if (-> args :options :help)
+        (help args)
+        (try
+          (let [wsdl (wsdl/parse (-> args :options :wsdl))]
+            (if-let [binding (-> args :options :binding)]
+              (if-let [operation (-> args :options :operation)]
+                (let [srv (get-in wsdl [binding :operations operation])]
+                  (do
+                    (println "\nService url:\n")
+                    (println (get-in wsdl [binding :url]))
+                    (println "\nSOAP action:\n")
+                    (println (service/soap-action srv))
+                    (println "\nRequest message:\n")
+                    (println (service/request-xml srv))
+                    (println "\nResponse message:\n")
+                    (println (service/response-xml srv))))
+                (print-table (select-keys wsdl [binding])))
+              (print-table wsdl))
+            (System/exit 0))
+          (finally
+            ;; Only called if `parse` raises an exception
+            (shutdown-agents)))))))
